3430ce68e9f0fdd7aa733e40a6588eb08409d1c9
Samir Sarkar <samir@Samirs-MacBook-Pro.local>
2020-05-28 23:32:52 +0530

    Showing posts from datebase instead of dummy posts

--- DJ/blogs/views.py
+++ DJ/blogs/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-posts = [
-    {
-        'author': 'Samir',
-        'title': 'Blog Post 1',
-        'content': 'This is just a dummy post 1',
-        'date_posted': 'May 28, 2020'
-    },
-    {
-        'author': 'Sankar',
-        'title': 'Blog Post 2',
-        'content': 'This is just a dummy post 2',
-        'date_posted': 'May 29, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blogs/home.html', context=context)
 

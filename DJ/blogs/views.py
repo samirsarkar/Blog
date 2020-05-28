68e94f3e3a2b2c44b14fa6cc9acbc07979be0004
Samir Sarkar <samir@Samirs-MacBook-Pro.local>
2020-05-28 21:59:27 +0530

    Aoplied the html and templates and also the added css file and bootstarp for better looking

--- DJ/blogs/views.py
+++ DJ/blogs/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Samir',
+        'title': 'Blog Post 1',
+        'content': 'This is just a dummy post 1',
+        'date_posted': 'May 28, 2020'
+    },
+    {
+        'author': 'Sankar',
+        'title': 'Blog Post 2',
+        'content': 'This is just a dummy post 2',
+        'date_posted': 'May 29, 2020'
+    }
+]
 
 
 def home(request):
-    return render(request, 'blogs/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blogs/home.html', context=context)
 
 
 def about(request):
-    return render(request, 'blogs/about.html')
+    return render(request, 'blogs/about.html', {'title': 'About'})

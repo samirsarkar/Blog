23635b9cde8347577e20b9d985d4ad9ac922da61
Samir Sarkar <samir@Samirs-MacBook-Pro.local>
2020-06-02 17:02:26 +0530

    Added functionality to create, edit, delete a post and applied authentication to do so

--- DJ/blogs/urls.py
+++ DJ/blogs/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home, name='blogs-home'),
+    # path('', views.home, name='blogs-home'),
+    path('', PostListView.as_view(), name='blogs-home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blogs-about'),
 
 ]
